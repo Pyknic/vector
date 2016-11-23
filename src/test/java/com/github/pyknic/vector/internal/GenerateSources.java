985513297c75afa7db5c8607766c18efda617a62
pyknic <emil@speedment.com>
2016-11-22 21:15:25 -0800

    Update generator to also create double types

--- src/test/java/com/github/pyknic/vector/internal/GenerateSources.java
+++ src/test/java/com/github/pyknic/vector/internal/GenerateSources.java
@@ -38,30 +38,31 @@ import static java.util.stream.Collectors.joining;
 public final class GenerateSources {
     
     private final static Path ROOT = Paths.get("src", "main", "java");
+    private final static Config[] CONFIGS = {
+        new Config("i", "int", "Integer", "Int", "", "", "", "%d", "(double)", "(int)"),
+        new Config("d", "double", "Double", "Double", "d", "Float.floatToIntBits((float) ", ")", "%.3f", "", "")
+    };
     
     public static void main(String... param) {
-        
-        final Config intConfig = new Config(
-            "i", "int", "Integer", "Int", "", "", "", "%d"
-        );
-        
-        for (final Class<?> prototype : PROTOTYPES) {
-            final Path path = ROOT.resolve(
-                prototype.getName().replace('.', '/') + ".java"
-            );
-            
-            try {
-                final String filename = intConfig.applyAllRules(
-                    path.getFileName().toString()
+        for (final Config config : CONFIGS) {
+            for (final Class<?> prototype : PROTOTYPES) {
+                final Path path = ROOT.resolve(
+                    prototype.getName().replace('.', '/') + ".java"
                 );
-                final Path target = path.getParent().resolve(filename);
-                final String content = Files.lines(path)
-                    .map(intConfig::applyAllRules)
-                    .collect(joining("\n"));
-                
-                Files.write(target, content.getBytes(UTF_8));
-            } catch (final IOException ex) {
-                throw new RuntimeException(ex);
+
+                try {
+                    final String filename = config.applyAllRules(
+                        path.getFileName().toString()
+                    );
+                    final Path target = path.getParent().resolve(filename);
+                    final String content = Files.lines(path)
+                        .map(config::applyAllRules)
+                        .collect(joining("\n"));
+
+                    Files.write(target, content.getBytes(UTF_8));
+                } catch (final IOException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         }
     }
@@ -73,7 +74,9 @@ public final class GenerateSources {
                                 SUFFIX         = "%5$s",
                                 TO_BITS_PREFIX = "%6$s",
                                 TO_BITS_SUFFIX = "%7$s",
-                                FORMAT         = "%8$s";
+                                FORMAT         = "%8$s",
+                                CAST_TO_DOUBLE = "%9$s",
+                                CAST_TO_FLOAT  = "%10$s";
     
     private final static Class<?>[] PROTOTYPES = {
         Vecf.class,
@@ -101,7 +104,8 @@ public final class GenerateSources {
         rule("Vec2f", "Vec2" + ACRONYM),
         rule("Vec3f", "Vec3" + ACRONYM),
         rule("Vec4f", "Vec4" + ACRONYM),
-        rule("%.3f", FORMAT),
+        rule("%\\.3f", FORMAT),
+        rule("\\(float\\)", CAST_TO_FLOAT),
         rule("Float\\.floatToIntBits\\(getX\\(\\)\\)", TO_BITS_PREFIX + "getX()" + TO_BITS_SUFFIX),
         rule("Float\\.floatToIntBits\\(getY\\(\\)\\)", TO_BITS_PREFIX + "getY()" + TO_BITS_SUFFIX),
         rule("Float\\.floatToIntBits\\(getZ\\(\\)\\)", TO_BITS_PREFIX + "getZ()" + TO_BITS_SUFFIX),
@@ -110,11 +114,10 @@ public final class GenerateSources {
         rule("Float\\.floatToIntBits\\(o\\.getY\\(\\)\\)", TO_BITS_PREFIX + "o.getY()" + TO_BITS_SUFFIX),
         rule("Float\\.floatToIntBits\\(o\\.getZ\\(\\)\\)", TO_BITS_PREFIX + "o.getZ()" + TO_BITS_SUFFIX),
         rule("Float\\.floatToIntBits\\(o\\.getW\\(\\)\\)", TO_BITS_PREFIX + "o.getW()" + TO_BITS_SUFFIX),
-        rule("float", PRIMITIVE),
+        rule("\\(double\\)", CAST_TO_DOUBLE),
+        rule("float ", PRIMITIVE + " "),
         rule("Float ", WRAPPER + " "),
-        rule("Float\\.", WRAPPER + "."),
         rule("Float\\)", WRAPPER + ")"),
-        rule("Float", UC_PRIMITIVE),
         rule("0f", "0" + SUFFIX),
         rule("1f", "1" + SUFFIX)
     };
@@ -143,7 +146,9 @@ public final class GenerateSources {
                 config.suffix,
                 config.toBitsPrefix,
                 config.toBitsSuffix,
-                config.format
+                config.format,
+                config.castToDouble,
+                config.castToFloat
             ));
         }
     }
@@ -158,6 +163,8 @@ public final class GenerateSources {
         private final String toBitsPrefix;
         private final String toBitsSuffix;
         private final String format;
+        private final String castToDouble;
+        private final String castToFloat;
 
         public Config(
                 String acronym, 
@@ -167,7 +174,9 @@ public final class GenerateSources {
                 String suffix,
                 String toBitsPrefix,
                 String toBitsSuffix,
-                String format) {
+                String format,
+                String castToDouble,
+                String castToFloat) {
             
             this.acronym      = acronym;
             this.primitive    = primitive;
@@ -177,6 +186,8 @@ public final class GenerateSources {
             this.toBitsPrefix = toBitsPrefix;
             this.toBitsSuffix = toBitsSuffix;
             this.format       = format;
+            this.castToDouble = castToDouble;
+            this.castToFloat  = castToFloat;
         }
         
         public String applyAllRules(String line) {
